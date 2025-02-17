db103c4656f2064e621f92f588bb41e52f2ae32e
omercakir2 <omercakir2323@gmail.com>
2025-02-17 16:51:52 +0300

    write a very basic glut code showing ascii codes of letters

--- main.cpp
+++ main.cpp
@@ -1,24 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <GL/glut.h>
 
-void display() {
-    // Set the background color to blue
-    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);  // Blue background (RGBA)
-    glClear(GL_COLOR_BUFFER_BIT);            // Clear the screen
+int ASCII = -1;
 
-    // Swap buffers (for double buffering)
-    glutSwapBuffers();
+void display()
+{
+   glClearColor(1, 0, 0, 0); // Red
+   glClear(GL_COLOR_BUFFER_BIT);
+   glutSwapBuffers();
 }
 
-int main(int argc, char **argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-    glutCreateWindow("First openGL");
+void onKey(unsigned char key, int x, int y)
+{
+   if (key == 27) // 27 means esc button
+   {
+      exit(0);
+   }
+   else if ((key >= 'a' && key <= 'z') && ASCII == 1)
+   {
+      printf("button's ascii code is %d \n", key);
+   }
+   if (key == 'a')
+   {
+      ASCII *= -1;
+   }
+}
+void onClick(int button, int state, int x, int y)
+{
+   if (button == GLUT_LEFT_BUTTON)
+      printf("LEFT BUTTON --");
+   else if (button == GLUT_RIGHT_BUTTON)
+      printf("RIGHT BUTTON --");
+   if (state == GLUT_DOWN)
+      printf("DOWN [%d %d]\n", x, y);
+   else
+      printf("UP [%d %d]\n", x, y);
+}
+void onSpecialKey(int key, int x, int y)
+{
+   switch (key)
+   {
+   case GLUT_KEY_UP:
+      printf("UP arrow pressed.\n");
+      break;
+   case GLUT_KEY_DOWN:
+      printf("DOWN arrow pressed.\n");
+      break;
+   case GLUT_KEY_LEFT:
+      printf("LEFT arrow pressed.\n");
+      break;
+   case GLUT_KEY_RIGHT:
+      printf("RIGHT arrow pressed.\n");
+      break;
+   }
+}
+int main(int argc, char *argv[])
+{
+   printf("Press ESC to exit the program\n");
+   printf("Click on the window with left or right mouse button.\n");
+   printf("Press arrow keys to be detected.\n");
+   printf("IF YOU WANNA LEARN ABOUT ASCII CODES USE LETTER 'a'\n");
 
-    // Set the window size
-    glutReshapeWindow(500, 500);  // You can adjust the size as needed
+   // CREATING A WINDOW
 
-    glutDisplayFunc(display);
-    glutMainLoop();
+   glutInit(&argc, argv);
+   glutInitWindowSize(500, 500);
+   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+   glutCreateWindow("First program!");
 
-    return 0;
+   // REGISTERING EVENTS
+   glutDisplayFunc(display);
+   glutKeyboardFunc(onKey);
+   glutSpecialFunc(onSpecialKey);
+   glutMouseFunc(onClick);
+
+   // START EVENT DISPATHER FORMING A LOOP UNTIL WE EXIT
+   glutMainLoop();
+
+   return 0;
 }
